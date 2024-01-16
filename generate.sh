688b35b7352d858a23a109abf5a4774a76f946d1
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2024-01-16 14:22:40 -0500

    Add report TeX files

--- generate.sh
+++ generate.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 
+CLINGO="$(which clingo)"
+
 for ((i=0;i<$2;i++))
 do
-    /cygdrive/c/Users/Admin/Downloads/clingo-5.3.0-win64/clingo.exe -n 1 --rand-freq=1 --seed=`echo $RANDOM` generator_$1.lp --stats | python3 parser.py
+    "${CLINGO}" -n 1 --rand-freq=1 --seed="${RANDOM}" generator_"${1}".lp --stats | python3 parser.py
 done
